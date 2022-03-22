ebf4beb107465cb94cff5098ed8880ffd655f310
Nakorn Thanhai <nakorn.thanhai@gmail.com>
2022-03-22 14:43:24 +0700

    make it better20

--- app.py
+++ app.py
@@ -5,6 +5,8 @@
 from flask import Flask, jsonify, render_template, request
 import json
 import numpy as np
+import pandas as pd
+import geopy.distance as ps
 
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
@@ -18,6 +20,8 @@ app = Flask(__name__)
 
 lineaccesstoken = 'dJtnbsqh9hkNLHAEmUIc66luvjc0hLu0IKHP4t1X7gxkbcJuhGWR9XTZX8/vD3ASvR7w0ufluAsmTVDkCqtLyyYS5Llknui2+1DjMJgRsUggppWc1+uM/2mi7sYbbXSOMo3qlET73lZLBak5b233AwdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(lineaccesstoken)
+
+casedata = pd.read_excel('casedata.xlsx')
 
 ####################### new ########################
 @app.route('/')
@@ -65,11 +69,42 @@ def event_handle(event):
         replyObj = TextSendMessage(text=msg)
         line_bot_api.reply_message(rtoken, replyObj)
 
+    if msgType == "location":
+        lat = event["message"]["latitude"]
+        lng = event["message"]["longitude"]
+        txtresult = handle_location(lat,lng,casedata,3)
+        replyObj = TextSendMessage(text=txtresult)
+        line_bot_api.reply_message(rtoken, replyObj)
+        
     else:
         sk_id = np.random.randint(1,17)
         replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
         line_bot_api.reply_message(rtoken, replyObj)
     return ''
+
+def handle_location(lat,lng,cdat,topK):
+    result = getdistace(lat, lng,cdat)
+    result = result.sort_values(by='km')
+    result = result.iloc[0:topK]
+    txtResult = ''
+    for i in range(len(result)):
+        kmdistance = '%.1f'%(result.iloc[i]['km'])
+        newssource = str(result.iloc[i]['News_Soruce'])
+        txtResult = txtResult + 'ห่าง %s กิโลเมตร\n%s\n\n'%(kmdistance,newssource)
+    return txtResult[0:-2]
+
+
+def getdistace(latitude, longitude,cdat):
+  coords_1 = (float(latitude), float(longitude))
+  ## create list of all reference locations from a pandas DataFrame
+  latlngList = cdat[['Latitude','Longitude']].values
+  ## loop and calculate distance in KM using geopy.distance library and append to distance list
+  kmsumList = []
+  for latlng in latlngList:
+    coords_2 = (float(latlng[0]),float(latlng[1]))
+    kmsumList.append(ps.vincenty(coords_1, coords_2).km)
+  cdat['km'] = kmsumList
+  return cdat
 
 if __name__ == '__main__':
     app.run(debug=True)
